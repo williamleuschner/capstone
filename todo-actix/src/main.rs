a6777518c04ddb10f809a98a004b07c26fa74186
William Leuschner <wel2138@rit.edu>
2021-07-08 23:55:21 -0400

    Actix Web todo list implementation

--- todo-actix/src/main.rs
+++ todo-actix/src/main.rs
@@ -1,39 +1,219 @@
-use actix_web::{get, post, web, App, HttpServer, HttpResponse, Responder};
-use chrono::Utc;
+use actix_files;
+use actix_web::{error, get, post, web, App, Error, HttpResponse, HttpServer, Responder};
+use chrono::{NaiveDate, Utc};
+use std::collections::HashMap;
+use std::ops::DerefMut;
+use std::sync::Mutex;
+use tera::{Context, Tera};
 use todo_list::{IncomingTodo, Todo, TodoList};
+use uuid::Uuid;
+
+struct AppStateWithTodoList {
+    list: Mutex<TodoList>,
+}
+
+fn redirect(to: &str) -> HttpResponse {
+    HttpResponse::Found().header("Location", to).finish()
+}
 
 #[get("/greeting/{name}")]
-async fn index(web::Path(name): web::Path<String>) -> impl Responder {
+async fn greet(web::Path(name): web::Path<String>) -> impl Responder {
     format!("Hello {}!", name)
 }
 
-#[post("/todo/create")]
-async fn todo(body: web::Bytes) -> impl Responder {
-    let title = std::str::from_utf8(&body).unwrap().to_owned();
-    let date = Utc::now();
-    let new = IncomingTodo {
-        title: title,
-        startable: date,
-        due: date,
+#[post("/new")]
+async fn todo(
+    todo_state: web::Data<AppStateWithTodoList>,
+    query: web::Form<HashMap<String, String>>,
+) -> Result<HttpResponse, Error> {
+    let success = if let Some(title) = query.get("title") {
+        if let Some(due_date) = query.get("due-date") {
+            if let Some(start_date) = query.get("start-date") {
+                let new = IncomingTodo {
+                    title: title.to_string(),
+                    startable: NaiveDate::parse_from_str(start_date, "%Y-%m-%d")
+                        .unwrap_or_else(|_| NaiveDate::from_ymd(1970, 1, 1)),
+                    due: NaiveDate::parse_from_str(due_date, "%Y-%m-%d")
+                        .unwrap_or_else(|_| NaiveDate::from_ymd(1970, 1, 1)),
+                };
+                let mut mutexed_todo_state = todo_state.list.lock().unwrap();
+                let mutexed_todos = mutexed_todo_state.deref_mut();
+                mutexed_todos.add(new);
+                true
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    } else {
+        false
     };
-    let mut todos = TodoList::new();
-    todos.add(new);
-    let all_todos = todos.get_all();
-    let new_json = serde_json::to_string(&all_todos);
 
-    match new_json {
-        Ok(result) => HttpResponse::Ok()
-            .content_type("application/json")
-            .body(result),
-        Err(_) => HttpResponse::InternalServerError().body("Whoops".to_owned())
+    if success {
+        Ok(redirect("/"))
+    } else {
+        Err(error::ErrorInternalServerError("Whoops"))
     }
+}
+
+#[post("/edit/{id}")]
+async fn update_todo(
+    web::Path(id): web::Path<String>,
+    todo_state: web::Data<AppStateWithTodoList>,
+    query: web::Form<HashMap<String, String>>,
+) -> Result<HttpResponse, Error> {
+    println!("POST /edit/{}", id);
+    let success = if let Some(title) = query.get("title") {
+        if let Some(due_date) = query.get("due-date") {
+            if let Some(start_date) = query.get("start-date") {
+                if let Ok(uuid) = Uuid::parse_str(&id) {
+                    let mut mutexed_todo_state = todo_state.list.lock().unwrap();
+                    let mutexed_todos = mutexed_todo_state.deref_mut();
+                    if let Some(existing) = mutexed_todos.get(uuid) {
+                        let updated = Todo {
+                            id: existing.id,
+                            title: title.to_string(),
+                            complete: existing.complete,
+                            startable: NaiveDate::parse_from_str(start_date, "%Y-%m-%d")
+                                .unwrap_or_else(|_| NaiveDate::from_ymd(1970, 1, 1)),
+                            due: NaiveDate::parse_from_str(due_date, "%Y-%m-%d")
+                                .unwrap_or_else(|_| NaiveDate::from_ymd(1970, 1, 1)),
+                        };
+                        mutexed_todos.update(updated);
+                        true
+                    } else {
+                        false
+                    }
+                } else {
+                    false
+                }
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    } else {
+        false
+    };
+
+    if success {
+        Ok(redirect("/"))
+    } else {
+        Err(error::ErrorInternalServerError("Whoops"))
+    }
+}
+
+#[get("/new")]
+async fn new_form(tera: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
+    let mut context = Context::new();
+    context.insert("action", "Create");
+    let s = tera
+        .render("detail.html.j2", &context)
+        .map_err(|e| error::ErrorInternalServerError(format!("Template error: {:?}", e)))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+#[get("/edit/{id}")]
+async fn edit_form(
+    web::Path(id): web::Path<String>,
+    todo_state: web::Data<AppStateWithTodoList>,
+    tera: web::Data<tera::Tera>,
+) -> Result<HttpResponse, Error> {
+    println!("GET /edit/{}", id);
+    if let Ok(uuid) = Uuid::parse_str(&id) {
+        let mut mutexed_todos_state = todo_state.list.lock().unwrap();
+        let mutexed_todos = mutexed_todos_state.deref_mut();
+        if let Some(this_todo) = mutexed_todos.get(uuid) {
+            let mut context = Context::new();
+            context.insert("todo", &this_todo);
+            context.insert("action", "Update");
+            let s = tera
+                .render("detail.html.j2", &context)
+                .map_err(|e| error::ErrorInternalServerError(format!("Template error: {:?}", e)))?;
+            Ok(HttpResponse::Ok().content_type("text/html").body(s))
+        } else {
+            Err(error::ErrorNotFound("unknown uuid"))
+        }
+    } else {
+        Err(error::ErrorNotFound("bad uuid"))
+    }
+}
+
+#[post("/complete/{id}")]
+async fn complete_todo(
+    web::Path(id): web::Path<String>,
+    todo_state: web::Data<AppStateWithTodoList>,
+) -> Result<HttpResponse, Error> {
+    println!("POST /complete/{}", id);
+    if let Ok(uuid) = Uuid::parse_str(&id) {
+        let mut mutexed_todos_state = todo_state.list.lock().unwrap();
+        let mutexed_todos = mutexed_todos_state.deref_mut();
+        mutexed_todos.toggle_completed(uuid);
+        let s = "{}";
+        Ok(HttpResponse::Ok().content_type("text/html").body(s))
+    } else {
+        Err(error::ErrorNotFound("unknown uuid"))
+    }
+}
+
+#[get("/")]
+async fn index(
+    todo_state: web::Data<AppStateWithTodoList>,
+    tera: web::Data<tera::Tera>,
+) -> Result<HttpResponse, Error> {
+    let mut context = Context::new();
+    let mut mutexed_todos = todo_state.list.lock().unwrap();
+    let all_todos = &mutexed_todos.deref_mut().get_all();
+    let mut today_todos = Vec::new();
+    let mut upcoming_todos = Vec::new();
+    let today = Utc::today().naive_utc();
+    for a_todo in all_todos {
+        if a_todo.startable <= today {
+            today_todos.push(a_todo);
+        } else {
+            upcoming_todos.push(a_todo);
+        }
+    }
+    context.insert("today_todos", &today_todos);
+    context.insert("upcoming_todos", &upcoming_todos);
+    let s = tera
+        .render("index.html.j2", &context)
+        .map_err(|e| error::ErrorInternalServerError(format!("Template error: {:?}", e)))?;
+    // let s = "hello world".to_owned();
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // let mut todos = TodoList::new();
-    HttpServer::new(|| App::new().service(index).service(todo))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    let todo_state = web::Data::new(AppStateWithTodoList {
+        list: Mutex::new(TodoList::new()),
+    });
+
+    HttpServer::new(move || {
+        let mut tera = match Tera::new("templates/*.j2") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        tera.autoescape_on(vec!["html.j2"]);
+
+        App::new()
+            // .app_data(todos.clone())
+            .app_data(todo_state.clone())
+            .data(tera)
+            .service(index)
+            .service(new_form)
+            .service(edit_form)
+            .service(todo)
+            .service(update_todo)
+            .service(complete_todo)
+            .service(actix_files::Files::new("/static", "./static"))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
--- todo-list/src/lib.rs
+++ todo-list/src/lib.rs
@@ -1,13 +1,13 @@
-use std::collections::HashMap;
+use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
-use chrono::{DateTime, Utc};
+use std::collections::HashMap;
 use uuid::Uuid;
 
 #[derive(Debug, Deserialize)]
 pub struct IncomingTodo {
     pub title: String,
-    pub startable: DateTime<Utc>,
-    pub due: DateTime<Utc>,
+    pub startable: NaiveDate,
+    pub due: NaiveDate,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -15,12 +15,12 @@ pub struct Todo {
     pub id: Uuid,
     pub title: String,
     pub complete: bool,
-    pub startable: DateTime<Utc>,
-    pub due: DateTime<Utc>,
+    pub startable: NaiveDate,
+    pub due: NaiveDate,
 }
 
 pub struct TodoList {
-    items: HashMap<Uuid, Todo>
+    items: HashMap<Uuid, Todo>,
 }
 
 impl TodoList {
@@ -29,7 +29,7 @@ impl TodoList {
             items: HashMap::new(),
         }
     }
-    pub fn add(&mut self, new: IncomingTodo) {
+    pub fn add(&mut self, new: IncomingTodo) -> Todo {
         let created = Todo {
             id: Uuid::new_v4(),
             title: new.title,
@@ -37,24 +37,43 @@ impl TodoList {
             startable: new.startable,
             due: new.due,
         };
+        let result = created.clone();
         self.items.insert(created.id, created);
+        result
     }
 
-    pub fn mark_completed(&mut self, id: Uuid) {
+    pub fn toggle_completed(&mut self, id: Uuid) {
         let maybe_item = self.items.get(&id);
+        println!("found item: {:?}", maybe_item);
         if let Some(item) = maybe_item {
-            let completed_item = Todo {
+            let toggled_item = Todo {
                 id: item.id,
                 title: item.title.clone(),
-                complete: true,
+                complete: !item.complete,
                 startable: item.startable,
                 due: item.due,
             };
-            self.items.insert(completed_item.id, completed_item);
+            println!("updated item: {:?}", toggled_item);
+            let old_item = self.items.insert(toggled_item.id, toggled_item);
+            println!("old item from hashmap: {:?}", old_item);
+            let gotten_item = self.items.get(&id);
+            println!("what you get from the hashmap: {:?}", gotten_item);
         };
     }
 
+    pub fn update(&mut self, existing: Todo) {
+        self.items.insert(existing.id, existing);
+    }
+
     pub fn get_all(&mut self) -> Vec<Todo> {
-        Vec::new()
+        let mut result = Vec::with_capacity(self.items.len());
+        for todo in self.items.values() {
+            result.push(todo.clone());
+        }
+        result
+    }
+
+    pub fn get(&mut self, id: Uuid) -> Option<Todo> {
+        self.items.get(&id).map(|v| v.clone())
     }
 }
