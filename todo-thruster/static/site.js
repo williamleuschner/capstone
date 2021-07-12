9bc32d072ccf330ee87a7bc7924617928b815efd
William Leuschner <wel2138@rit.edu>
2021-07-12 16:38:43 -0400

    Copy resources from actix to thruster

---
+++ todo-thruster/static/site.js
@@ -0,0 +1,18 @@
+(function() {
+	"use strict";
+
+	function toggleTodo(event) {
+		let id = event.target.parentElement.id;
+		const options = {
+			method: "POST"
+		};
+		fetch("/complete/" + id, options);
+	}
+
+	window.addEventListener("load", function() {
+		let elements = document.getElementsByClassName("todo-checkbox");
+		for (let el of elements) {
+			el.addEventListener("click", toggleTodo);
+		}
+	});
+})();
